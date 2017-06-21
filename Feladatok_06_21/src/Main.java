edd0d069d5ca41c9b36e993ae76852af09731452
HPeti <hpeti0912@gmail.com>
2017-06-21 14:43:59 +0200

    added anagramma

--- Feladatok_06_21/src/Main.java
+++ Feladatok_06_21/src/Main.java
@@ -1,10 +1,8 @@
-import sun.net.www.content.text.Generic;
-
 /**
  * Created by Student on 6/21/2017.
  */
 public class Main {
-    public static void anagramma(String s1, String s2)
+    public static String anagramma(String s1, String s2)
     {
         if(s2.length()<s1.length())
         {
@@ -14,15 +12,22 @@ public class Main {
         }
         char[] szo1=s1.toCharArray();
         char[] szo2=s2.toCharArray();
-        boolean bool=true;
         for (int i=0; i<szo1.length; i++)
         {
+            boolean volt=false;
             for (int j=0; j<szo2.length; j++)
             {
-
+                if(szo1[i]==szo2[j])
+                {
+                    volt= true;
+                }
+                if(j== szo2.length-1 && volt==false)
+                {
+                    return "nem az";
+                }
             }
         }
-
+        return "az";
     }
     public static void fibonacci(int szam) {
         int [] sorozat= new int[szam];
@@ -63,8 +68,9 @@ public class Main {
     {
         System.out.println("Feladat 1: anagramma");
         String szo1="abc";
-        String szo2="dcba";
-//        anagramma(szo1,szo2);
+        String szo2="abcd";
+        System.out.println(szo1+" "+szo2);
+        System.out.println(anagramma(szo1,szo2));
 
         System.out.println("Feladat 2: fibonacci");
         fibonacci(10);
