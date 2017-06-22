cde07cb1478a10ab1bc6986311e548864f0daac5
HPeti <hpeti0912@gmail.com>
2017-06-22 16:03:30 +0200

    Calc. need to be fixed

--- Feladatok_06_22/src/Main.java
+++ Feladatok_06_22/src/Main.java
@@ -44,46 +44,63 @@ public class Main {
     }
     public static void calculator(String kifejezes) {
         List<String> lista = Arrays.asList(kifejezes.split(" "));
-//        int muvelet=0;
-//        for (String item : lista)
-//        {
-//            if(!Character.isDigit(item.charAt(0)))
-//            {
-//                muvelet++;
-//            }
-//        }
+        String eredmeny="";
+        int muvelet=0;
+        for (String item : lista)
+        {
+            if(!Character.isDigit(item.charAt(0)));
+            {
+                muvelet++;
+            }
+        }
             for (String item : lista) {
                 int temp=0;
+                int index=lista.indexOf(item);
                 if (Character.isDigit(item.charAt(0)) && item != null) {
 
                 } else {
                     switch (item) {
                         case "+": {
-//                            temp=Integer.parseInt(item+Integer.parseInt(item[temp+1]);
-//                            item[temp-1]=Integer.toString(temp);
-//                            item[temp]=Integer.toString(temp);
-//                            item[temp+1]=Integer.toString(temp);
+                            temp=Integer.parseInt(lista.get(index-1))+Integer.parseInt(lista.get(index+1));
+                            lista.set(index-1,Integer.toString(temp));
+                            lista.set(index,Integer.toString(temp));
+                            lista.set(index+1,Integer.toString(temp));
                             break;
                         }
                         case "-": {
-
+                            temp=Integer.parseInt(lista.get(index-1))-Integer.parseInt(lista.get(index+1));
+                            lista.set(index-1,Integer.toString(temp));
+                            lista.set(index,Integer.toString(temp));
+                            lista.set(index+1,Integer.toString(temp));
                             break;
                         }
                         case "*": {
-
+                            temp=Integer.parseInt(lista.get(index-1))*Integer.parseInt(lista.get(index+1));
+                            lista.set(index-1,Integer.toString(temp));
+                            lista.set(index,Integer.toString(temp));
+                            lista.set(index+1,Integer.toString(temp));
                             break;
                         }
                         case "/": {
-
+                            temp=Integer.parseInt(lista.get(index-1))/Integer.parseInt(lista.get(index+1));
+                            lista.set(index-1,Integer.toString(temp));
+                            lista.set(index,Integer.toString(temp));
+                            lista.set(index+1,Integer.toString(temp));
                             break;
                         }
                     }
+                }
+                muvelet--;
+                if(muvelet==0)
+                {
+                    eredmeny=item;
                 }
             }
             for (String item: lista)
             {
                 System.out.print(item+" ");
             }
+            System.out.println("\n"+eredmeny);
     }
     public static void kozosResz(String [] tomb) {
 
@@ -92,14 +109,14 @@ public class Main {
     public static void main(String [] args)
     {
         Scanner scanner=new Scanner(System.in);
-//        System.out.println("Feladat 1: Zárójelek");
-//        System.out.print("\nAdd meg a zárójeleket: ");
-//        System.out.println(zarojel(scanner.nextLine()));
+        System.out.println("Feladat 1: Zárójelek");
+        System.out.print("\nAdd meg a zárójeleket: ");
+        System.out.println(zarojel(scanner.nextLine()));
 
         System.out.println("Feladat 2: Számológép");
-        System.out.print("\nAdj meg egy aritmetikai kifejezést");
+        System.out.print("\nAdj meg egy aritmetikai kifejezést"); //a sorrendet meg kell valósítani
         calculator(scanner.nextLine());
-//        calculator(scanner.nextLine());
+
 //        System.out.println("Feladat 3: Közös rész a stringbe");
 
     }
