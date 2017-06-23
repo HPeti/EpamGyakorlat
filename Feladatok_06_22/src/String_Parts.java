1e6a5d007a25cfaf6e3c2ea1a7b92b39d7e975a8
HPeti <hpeti0912@gmail.com>
2017-06-23 11:18:47 +0200

    Build finished, commonPart method added

--- Feladatok_06_22/src/String_Parts.java
+++ Feladatok_06_22/src/String_Parts.java
@@ -2,8 +2,21 @@
  * Created by Student on 6/23/2017.
  */
 public class String_Parts {
-    public static void kozosResz(String text) {
+    public static void commonPart(String text) {
         String [] tomb=text.split(" ");
+        String common= tomb[0];
+        for (int i=0; i<tomb.length;i++)
+        {
+            for (int j=0;j<common.length();j++)
+            {
+                if (tomb[i].charAt(j)!=common.charAt(j))
+                {
+                    common=common.substring(0,j-1);
+                    break;
+                }
+            }
+        }
+        System.out.println("A közös rész: "+common);
 
     }
 
