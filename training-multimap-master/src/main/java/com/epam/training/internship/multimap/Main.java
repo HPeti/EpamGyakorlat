0c182b0ceb36b1f67adb50a71b97d1df010ab776
Peter Hegedus2 <Peter_Hegedus2@epam.com>
2017-07-04 16:01:03 +0200

    input needed

--- training-multimap-master/src/main/java/com/epam/training/internship/multimap/Main.java
+++ training-multimap-master/src/main/java/com/epam/training/internship/multimap/Main.java
@@ -10,13 +10,79 @@ public class Main {
     {
         Scanner scanner= new Scanner(System.in);
         MultiMap<String, Integer> multiMap = new MultiMapClass<String, Integer>();
-        System.out.print("\n1 - kulcs-érték pár hozzáadása <put>(key, value)");
-        System.out.print("\n2 - Érték visszaadása <get>(key)");
-        System.out.print("\n3 - ");
-        System.out.print("\n4 - ");
-        System.out.print("\n5 - ");
-        System.out.print("\nAdd meg a végrehajtandó parancsot:");
         int action=0;
+        boolean exit=false;
+        while (!exit)
+        {
+            System.out.print("\n1 - Kulcs-érték pár hozzáadása <put>(key, value)");
+            System.out.print("\n2 - Érték visszaadása <get>(key)");
+            System.out.print("\n3 - Értékek eltávolítása kulcs alapján <remove>(Object key)");
+            System.out.print("\n4 - Kulcs-érték eltávolítása <remove>(Key, value)");
+            System.out.print("\n5 - Map clear");
+            System.out.print("\n6 - Key létezik-e <containsKey>(Key)");
+            System.out.print("\n7 - Value létezik-e <containsValue>(Value)");
+            System.out.print("\n8 - Map üres-e");
+            System.out.print("\n9 - Map mérete");
+            System.out.print("\n10 - Map listázása");
+            System.out.print("\n0 - Program kilépése");
+            System.out.print("\nAdd meg a végrehajtandó parancsot:");
+            switch (action=Integer.parseInt(scanner.nextLine()))
+            {
+                case 1:{
+                    System.out.print("\nAdd meg az értéket:");
+                    break;
+                }
+                case 2:{
+                    System.out.print("\nAdd meg az értéket:");
+                    break;
+                }
+                case 3:{
+                    System.out.print("\nAdd meg az értéket:");
+                    break;
+                }
+                case 4:{
+                    System.out.print("\nAdd meg az értéket:");
+                    break;
+                }
+                case 5:{
+                    System.out.print("\nA map törölve");
+                    multiMap.clear();
+                    break;
+                }
+                case 6:{
+                    System.out.print("\nAdd meg az értéket:");
+                    break;
+                }
+                case 7:{
+                    System.out.print("\nAdd meg az értéket:");
+                    int value=Integer.parseInt(scanner.nextLine());
+                    multiMap.containsValue(value);
+                    break;
+                }
+                case 8:{
+                    System.out.print("\n"+multiMap.isEmpty());
+                    break;
+                }
+                case 9:{
+                    System.out.print("\n"+multiMap.size());
+                    break;
+                }
+                case 0:{
+                    exit=true;
+                    break;
+                }
+                default:
+                {
+                    exit=true;
+                    break;
+                }
+
+
+
+            }
+        }
+
+
 
 
     }
